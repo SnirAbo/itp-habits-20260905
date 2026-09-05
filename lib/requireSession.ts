ae1f0f898ba10c564be5489d3b2a89975fc51b2f
Snir Abo <160028687+SnirAbo@users.noreply.github.com>
2026-09-05 09:46:02 +0300

    chore: add requireSession helper

---
+++ lib/requireSession.ts
@@ -0,0 +1,8 @@
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/lib/auth';
+
+export async function requireSession() {
+  const session = await getServerSession(authOptions);
+  if (!session) throw new Error('Unauthorized');
+  return session as any as { userId: number };
+}
